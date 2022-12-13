e2389c29ba6ed3a1196abea3be70bf8095bf2ef2
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-13 08:13:27 +0530

    Update main.c

--- main.c
+++ main.c
@@ -5,5 +5,9 @@
 int main() {
   // Test batterOk for success
   assert(batteryIsOk(25, 70, 0.7) == 1);
+  
+  // Test for temperature not in Range, soc and charge current in range
+  assert(batteryIsOk(50, 70, 0.5) == 0 );
+  
   return 0;
 }
