3a7867a014beccc7b018f04522b0fdb83538b174
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-13 08:55:03 +0530

    Update main.c
    updated the test cases

--- main.c
+++ main.c
@@ -4,13 +4,25 @@
 
 int main() {
   // Test for temperature not in Range, soc and charge current in range
-  assert(batteryIsOk(25, 70, 0.7) == 1);
+  assert(batteryIsOk(25, 78, 0.7) == 1);
   
-  // Test for temperature not in Range, soc and charge current in range
-  assert(batteryIsOk(52, 70, 0.6) == 0);
+  // Test for soc and charge current in range and temperature out of Range
+  assert(batteryIsOk(52, 78, 0.6) == 0);
   
   // Test for temperature, charge current in range and soc out of range
   assert(batteryIsOk(30, 88, 0.6) == 0);
+  
+  // Test for temperature, soc in range and charge current out of range
+  assert(batteryIsOk(35, 78, 0.9) == 0);
+  
+  // Test temperature range check function 
+  assert(checkTemperatureRange(30) == 1);
+  assert(checkTemperatureRange(42) == 0);
+  assert(checkTemperatureRange(0) == 1);
+  assert(checkTemperatureRange(40) == 1);
+  assert(checkTemperatureRange(-1) == 0);
+  
+  
   
   return 0;
 }
