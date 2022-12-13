c57ee8f34972a9afc5164f5eaed073e174e11191
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-13 08:48:09 +0530

    Update main.c
    updated the test cases

--- main.c
+++ main.c
@@ -3,11 +3,14 @@
 #include "checkFunctions.h"
 
 int main() {
-  // Test batterOk for success
+  // Test for temperature not in Range, soc and charge current in range
   assert(batteryIsOk(25, 70, 0.7) == 1);
   
   // Test for temperature not in Range, soc and charge current in range
-  assert(batteryIsOk(50, 70, 0.5) == 0 );
+  assert(batteryIsOk(52, 70, 0.5) == 0);
+  
+  // Test for temperature, charge current in range and soc out of range
+  assert(batteryIsOk(30, 88, 0.5) == 0);
   
   return 0;
 }
