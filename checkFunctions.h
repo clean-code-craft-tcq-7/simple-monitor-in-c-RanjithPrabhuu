9beaeed37612544efcec524c0da0513627edee11
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-13 07:46:47 +0530

    Create checkFunctions.h
    checkFunctions.h is updated

---
+++ checkFunctions.h
@@ -0,0 +1,6 @@
+//function declaration for check temperature in range check
+int checkTemperatureRange(float temperature);
+//function declaration for state of charge in range check
+int checkSocRange(float soc);
+//function declaration for charge rate in range check
+int checkChargeRateRange(float chargeRate);
