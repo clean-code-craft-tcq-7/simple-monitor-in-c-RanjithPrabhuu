7b1e4c65a5e482ac5f85eecd8517af600b57b207
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-19 14:27:31 +0530

    Update checkFunctions.c
    adapted the check functions

--- checkFunctions.c
+++ checkFunctions.c
@@ -2,34 +2,41 @@
 #include "checkFunctions.h"
 
 //function to check whether temperature is in range or not
-int checkTemperatureRange(float temperature)
+int checkTemperatureRange(float temperature, (void)(*printFunc)(int checkStatus), int warningLevelOn)
 {
-  if(temperature < 0 || temperature > 45) 
+  int retStatus = 1;
+  valueRangeStatus checkStatus = chackValueRange(TEMPERATURE_MIN_VALUE, TEMPERATURE_MAX_VALUE, temperature, warningLevelOn);
+  
+  if(checkStatus > NORMAL)
   {
-    printf("%s\n","Temperature out of range");
-    return 0;
+	  retStatus = 0;
+	  printFunc(checkStatus)
   }
-  return 1;
+  return retStatus;
 }
 
 //functions to check whether state of charge is in range or not
-int checkSocRange(float soc)
+int checkSocRange(float soc, (void)(*printFunc)(int checkStatus), int warningLevelOn)
 {
-  if(soc < 20 || soc > 80) 
+  int retStatus = 1;
+  valueRangeStatus checkStatus = chackValueRange(SOC_MIN_VALUE, SOC_MAX_VALUE, soc, warningLevelOn);
+  
+  if(checkStatus > NORMAL)
   {
-    printf("%s\n","State of Charge out of range!");
-    return 0;
+	  retStatus = 0;
+	  printFunc(checkStatus)
   }
-  return 1;
+  return retStatus;
 }
 
 //function to check whether charge rate is in range or not
-int checkChargeRateRange(float chargeRate)
+int checkChargeRateRange(float chargeRate, (void)(*printFunc)(int checkStatus), int warningLevelOn)
 {
-  if(chargeRate > 0.8) 
-  {
-    printf("%s\n","Charge Rate out of range!");
-    return 0;
-  }
-  return 1;
+   int retStatus = 1;
+   if(chargeRate > CHARGE_RATE_MAX_VALUE)
+   {
+    printFunc(HIGH_VALUE_BREAH);
+    retStatus = 0;
+   }
+  return retStatus;
 }
