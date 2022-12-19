d1bc66a0e8b978d65338e831991847e333f6d529
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-19 14:28:45 +0530

    Update checkFunctions.h
    updated the functions declaration

--- checkFunctions.h
+++ checkFunctions.h
@@ -1,6 +1,6 @@
 //function declaration for check temperature in range check
-int checkTemperatureRange(float temperature);
+int checkTemperatureRange(float temperature,(void)(*printFunc)(int checkStatus), int warningLevelOn);
 //function declaration for state of charge in range check
-int checkSocRange(float soc);
+int checkSocRange(float soc,(void)(*printFunc)(int checkStatus), int warningLevelOn);
 //function declaration for charge rate in range check
-int checkChargeRateRange(float chargeRate);
+int checkChargeRateRange(float chargeRate, (void)(*printFunc)(int checkStatus), int warningLevelOn);
