1aae2e683865becb9d208035c1219cf8adee1357
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-19 15:19:26 +0530

    Create config.h
    updated the config

---
+++ config.h
@@ -0,0 +1,13 @@
+typedef enum{
+	ENGLISH,
+	GERMAN,
+	MAX_LANGUAGE_SUPRTD,
+}langType;
+
+#define TEMPERATURE_MIN 10
+#define TEMPERATURE_MAX	45
+
+#define SOC_MIN 20
+#define SOC_MAX 80
+
+#define CHARGE_RATE_MAX\ 0.8
