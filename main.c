697982be7d028cba7315d83aba152866cf793943
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-19 16:18:37 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,37 +3,15 @@
 #include "checkFunctions.h"
 
 int main() {
+ 
+  // Test for temperature, soc, charge current in range
+  assert(batteryIsOk(25, 70, 0.7, 1));
   // Test for temperature not in Range, soc and charge current in range
-  assert(batteryIsOk(25, 78, 0.7) == 1);
-  
-  // Test for soc and charge current in range and temperature out of Range
-  assert(batteryIsOk(52, 78, 0.6) == 0);
-  
+  assert(!batteryIsOk(50, 70, 0.5,1));
   // Test for temperature, charge current in range and soc out of range
-  assert(batteryIsOk(30, 88, 0.6) == 0);
-  
+  assert(!batteryIsOk(30, 85, 0.5, 1));
   // Test for temperature, soc in range and charge current out of range
-  assert(batteryIsOk(35, 78, 0.9) == 0);
-  
-  // Test temperature range check function 
-  assert(checkTemperatureRange(42) == 1);
-  assert(checkTemperatureRange(47) == 0);
-  assert(checkTemperatureRange(0) == 1);
-  assert(checkTemperatureRange(40) == 1);
-  assert(checkTemperatureRange(-1) == 0);
-  
-  // Test soc range check function
-  assert(checkSocRange(19.5) == 0);
-  assert(checkSocRange(20) == 1);
-  assert(checkSocRange(40) == 1);
-  assert(checkSocRange(80) == 1);
-  assert(checkSocRange(82) == 0);
-  
-  // Test charge Rate check function
-  assert(checkChargeRateRange(0.1) == 1);
-  assert(checkChargeRateRange(0.81) == 0);
-  assert(checkChargeRateRange(0.9) == 0);
-  assert(checkChargeRateRange(1.0) == 0);
+  assert(!batteryIsOk(30, 70, 0.9, 1));
   
   return 0;
 }
