fb3f9b5685ad0ede8579f964a6bb73e4fcbdf4c0
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-19 14:59:24 +0530

    Update checkFunctions.c
    updated the function call

--- checkFunctions.c
+++ checkFunctions.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "checkFunctions.h"
+#include "checkValues.h"
 
 //function to check whether temperature is in range or not
 int checkTemperatureRange(float temperature, (void)(*printFunc)(int checkStatus), int warningLevelOn)
 {
   int retStatus = 1;
-  valueRangeStatus checkStatus = chackValueRange(TEMPERATURE_MIN_VALUE, TEMPERATURE_MAX_VALUE, temperature, warningLevelOn);
+  valueRange checkStatus = checkValueRange(TEMPERATURE_MIN_VALUE, TEMPERATURE_MAX_VALUE, temperature, warningLevelOn);
   
   if(checkStatus > NORMAL)
   {
@@ -19,7 +20,7 @@ int checkTemperatureRange(float temperature, (void)(*printFunc)(int checkStatus)
 int checkSocRange(float soc, (void)(*printFunc)(int checkStatus), int warningLevelOn)
 {
   int retStatus = 1;
-  valueRangeStatus checkStatus = chackValueRange(SOC_MIN_VALUE, SOC_MAX_VALUE, soc, warningLevelOn);
+  valueRange checkStatus = checkValueRange(SOC_MIN_VALUE, SOC_MAX_VALUE, soc, warningLevelOn);
   
   if(checkStatus > NORMAL)
   {
