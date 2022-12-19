2f431bdb29b61e1d31714d08332c4acf7042e79b
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-19 14:16:12 +0530

    Update checker.c
    extended the battery function

--- checker.c
+++ checker.c
@@ -4,13 +4,13 @@
 #include "checkFunctions.h"
 
 //function declaration for battery is ok check 
-int batteryIsOk(float temperature, float soc, float chargeRate) 
+int batteryIsOk(float temperature, float soc, float chargeRate, int warningLevelOn) 
 {
     int returnStatus = 0;
   
-    returnStatus = checkTemperatureRange(temperature);
-    returnStatus = (returnStatus & checkSocRange(soc));
-    returnStatus = (returnStatus & checkChargeRateRange(chargeRate));
+    returnStatus = checkTemperatureRange(temperature,&tempRangeDisplayInfo,warningLevelOn);
+    returnStatus = (returnStatus & checkSocRange(soc,&socpRangeDisplayInfo,warningLevelOn));
+    returnStatus = (returnStatus & checkChargeRateRange(chargeRate,&chargeRateRangeDisplayInfo,warningLevelOn));
     
     return returnStatus;
  } 
