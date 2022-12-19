5b3885e2723d531d14cdab2e3a9059fea4a5e9bf
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-19 14:48:23 +0530

    Create checkValues.h
    updated the fucntion declaration and enums

---
+++ checkValues.h
@@ -0,0 +1,12 @@
+typedef enum
+{
+	NORMAL = 1,
+	LOW_VALUE_WARNING,
+	HIGH_VALUE_WARNING,
+	LOW_VALUE_BREACH,
+	HIGH_VALUE_BREACH,
+} valueRange;
+
+valueRange checkMinMaxBreach(float min, float max, float value);
+valueRange checkWarning(float minTolerance,float maxTolerance, float value);
+valueRange chackValueRange(float min, float max, float value, float tolerance,int warningLevelOn);
