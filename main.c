435433d2c4de6ba1fcf03fdb113859951f79d09f
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-20 12:55:17 +0530

    Update main.c
    updated the test cases

--- main.c
+++ main.c
@@ -4,21 +4,21 @@
 
 int main() 
 {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
-  assert(batteryIsOk(44, 79, 0.7));
-  assert(!batteryIsOk(46, 81, 0.8));
-  assert(!batteryIsOk(0, 0, 0.7));
-  SelectLanguageandWarnigMessage(DEFAULT);
-  assert(Store_WarningMessage[(BatteryHelathMonitor(13))] == "LOW_SOC_BREACH");
-  SelectLanguageandWarnigMessage(GERMAN);
-  assert(Store_WarningMessage[(BatteryHelathMonitor(77))] == "HIGH_SOC_WARNUNG");
-  assert(PreWarningMessageTable[PreWarningIndicatorMessage(23,SOCMIN,SOCMAX)] == "Approaching_discharge"); /* 5% of 80 is 4 , [20-24],[76-80] */
-  assert(PreWarningMessageTable[PreWarningIndicatorMessage(77,SOCMIN,SOCMAX)] == "Approaching_charge_peak");
-  assert(PreWarningMessageTable[PreWarningIndicatorMessage(1,TEMPMIN,TEMPMAX)] == "Approaching_discharge"); /* 5% of 45 is 2 , [0-2],[43-45] */
-  assert(PreWarningMessageTable[PreWarningIndicatorMessage(44,TEMPMIN,TEMPMAX)] == "Approaching_charge_peak");
-  SelectLanguageandWarnigMessage(CHINA);
-  assert(Store_WarningMessage[(BatteryHelathMonitor(23))] == "L_S_WARN!!");
+  assert(batteryIsOk(25, 70, 0.7) == 1);
+  assert(batteryIsOk(50, 85, 0) == 0 );
+  assert(batteryIsOk(44, 79, 0.7) == 1);
+  assert(batteryIsOk(46, 81, 0.8) == 0);
+  assert(batteryIsOk(0, 0, 0.7) == 0);
+ 
+  checkLanguage(ENGLISH);
+  assert(breachMessage[(checkBatteryHealth(77,24,76))] == "HIGH_SOC_WARNING");
+  checkLanguage(GERMAN);
+  assert(breachMessage[(checkBatteryHealth(23,24,76))] == "Niedrige SOC-Warnung");
+  
+  assert(WarningMessage[checkWarningLevel(23,SOC_MIN ,SOC_MAX)] == "ReachingMinThreshold"); // 5% of 80 is 4 , [20-24],[76-80]
+  assert(WarningMessage[checkWarningLevel(77,SOC_MIN,SOC_MAX)] == "ReachingMaxThreshold");
+  assert(WarningMessage[checkWarningLevel(1,TEMPERATURE_MIN ,TEMPERATURE_MAX )] == "ReachingMinThreshold"); // 5% of 45 is 2 , [0-2],[43-45]
+  assert(WarningMessage[checkWarningLevel(44,TEMPERATURE_MIN,TEMPERATURE_MIN)] == "ReachingMaxThreshold");
   
   return 0;
 }
