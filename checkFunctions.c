22c79d832140ddf9f61d0c2f2d6ad74aa38e89f5
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-20 07:46:20 +0530

    Update checkFunctions.c

--- checkFunctions.c
+++ checkFunctions.c
@@ -15,7 +15,7 @@ int checkChargeRate(float value)
    }
 }
 
-/Function that checks whether input value is in range or not
+//Function that checks whether input value is in range or not
 int checkRange(float value,float minVal,float maxVal)
 {
     if((value < minVal) || (value > maxVal))
