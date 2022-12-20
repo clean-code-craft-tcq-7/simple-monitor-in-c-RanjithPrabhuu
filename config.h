54fb87169196da8284a90f3dbf46ccc114bc5a4e
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-20 07:16:02 +0530

    Update config.h
    updated the macro

--- config.h
+++ config.h
@@ -1,13 +1,6 @@
-typedef enum{
-	ENGLISH,
-	FRENCH,
-	MAX_LANGUAGE_SUPRTD,
-}langType;
 
 #define TEMPERATURE_MIN 10
 #define TEMPERATURE_MAX	45
-
 #define SOC_MIN 20
 #define SOC_MAX 80
-
 #define CHARGE_RATE_MAX 0.8
