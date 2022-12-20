ed14a2b7bb23f9eb5541ab46d4d6c7a0383e3dd4
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-20 08:11:42 +0530

    Update checkConfig.h

--- checkConfig.h
+++ checkConfig.h
@@ -1,6 +1,24 @@
-
+//Macro for min max range
 #define TEMPERATURE_MIN 10
 #define TEMPERATURE_MAX	45
 #define SOC_MIN 20
 #define SOC_MAX 80
 #define CHARGE_RATE_MAX 0.8
+
+//enum for langType
+typedef enum 
+{
+    ENGLISH,
+    GERMAN,
+	  MAX_LANG
+}langType;
+
+//enum for warning messages
+typedef enum {
+    LOW_SOC_WARNING,
+	  HIGH_SOC_WARNING,
+	  LOW_SOC_BREACH,
+	  HIGH_SOC_BREACH,
+    MESSAGE_MAX
+} warningType;
+
