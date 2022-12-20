72e79b26a5ce38e4a569324cac666b7a4ae770a9
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-20 07:49:06 +0530

    Update checker.c
    updated the battery function

--- checker.c
+++ checker.c
@@ -2,42 +2,16 @@
 #include <assert.h>
 #include "checker.h"
 #include "checkFunctions.h"
-#include "config.h"
+#include "checkConfig.h"
 
-
-/*Function Delceration for BMS*/
-int Check_ChargeRate(float chargeRate);
-
-/*Function Returns True if the Battery is OK else return false*/
+//function declaration for battery is ok check 
 int batteryIsOk(float temperature, float soc, float chargeRate)
 {
-  int batterystate = 1;
-  /*Macro returns flase if the Temperature is out of range and true if Temperature is in range*/ 
-  batterystate = RangeCheck(temperature,TEMPMIN,TEMPMAX);
-  /*Macro returns flase if the SOC is out of range and true if its in range*/
-  batterystate *= RangeCheck(soc,SOCMIN,SOCMAX);
-  batterystate *= Check_ChargeRate(chargeRate);
-
-  return batterystate;
+   int returnStatus = 0;
+  
+    returnStatus = checkRange(temperature,TEMPERATURE_MIN,TEMPERATURE_MAX);
+    returnStatus = (returnStatus & checkRange(soc,SOC_MIN,SOC_MAX));
+    returnStatus = (returnStatus & checkChargeRate(chargeRate));
+    
+    return returnStatus;
 }
- 
-/*Function returns flase if the chargeRate is out of range and true if its in range*/ 
-int Check_ChargeRate(float chargeRate)
-{
-   if(chargeRate > CHARGERATEMAX)
-   return 0;
-   else
-   return 1;
-}
-
-void SelectLanguageandWarnigMessage(Select_Language Language)
-{   
-   if(Language < MAX_LANG)
-   {
-      for(int i =0;i<MESSAGE_MAX;i++)
-      {
-	Store_WarningMessage[i] = WarningMessageTable[Language][i];
-      }
-    } 
-}
-
