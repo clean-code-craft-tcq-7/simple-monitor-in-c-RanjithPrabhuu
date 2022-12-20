ec5b9a6d38b2dfab25a02c6c96318fa9c3cb77ee
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-20 07:21:55 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -4,21 +4,6 @@
 #include "checkFunctions.h"
 #include "config.h"
 
-const char* Store_WarningMessage[MESSAGE_MAX];
-const char* PreWarningMessageTable[Warning_max] = {"Approaching_discharge","Approaching_charge_peak"};
-const char* WarningMessageTable[MAX_LANG][MESSAGE_MAX] = {
-		{/*Language : DEFAULT */
-			"LOW_SOC_BREACH","LOW_SOC_WARNING","SOC_NORMAL","HIGH_SOC_WARNING","HIGH_SOC_BREACH","SOC_UNDEFINED"},
-		{/*Language : ENGLISH */
-			"LOW_SOC_BREACH","LOW_SOC_WARNING","SOC_NORMAL","HIGH_SOC_WARNING","HIGH_SOC_BREACH","SOC_UNDEFINED"},
-		{/*Language : GERMAN */
-			"LOW_SOC_BREACH","LOW_SOC_WARNUNG","SOC_NORMAL","HIGH_SOC_WARNUNG","HIGH_SOC_BREACH","SOC_UNDEFINED"},
-		{ /*Language : CHINA */
-			"L_S_BREACHEN","L_S_WARN!!","NORMAL","H_S_WARN!!","H_S_BREACHEN","UNDEFINED"}
-		/* Extend the table as per Requirements  */
-		
-	};
-
 
 /*Function Delceration for BMS*/
 int Check_ChargeRate(float chargeRate);
