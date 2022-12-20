d2c0e35df5980c6594095dd9da197165967600df
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-20 07:46:33 +0530

    Update checkFunctions.h

--- checkFunctions.h
+++ checkFunctions.h
@@ -1,4 +1,5 @@
-int checkTemperatureRange(float temperature,(void)(*printFunc)(int checkStatus), int warningLevelOn);
-int checkSocRange(float soc,(void)(*printFunc)(int checkStatus), int warningLevelOn);
-int checkChargeRateRange(float chargeRate,(void)(*printFunc)(int checkStatus), int warningLevelOn);
+//Function that checks whether charge rate is in range 
+int checkChargeRate(float value);
+//Function that checks whether input value is in range or not
+int checkRange(float value,float minVal,float maxVal);
 
