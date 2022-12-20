f6b67f518bcf4bcf3f7a15a1ed423a3b4cfc4bf9
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-20 08:17:16 +0530

    Update checkFunctions.h

--- checkFunctions.h
+++ checkFunctions.h
@@ -1,5 +1,10 @@
+#include "stdio.h"
+#include "checkConfig.h"
+
 //Function that checks whether charge rate is in range 
 int checkChargeRate(float value);
 //Function that checks whether input value is in range or not
 int checkRange(float value,float minVal,float maxVal);
+//function to select the language type
+void setLanguage(langType Language);
 
