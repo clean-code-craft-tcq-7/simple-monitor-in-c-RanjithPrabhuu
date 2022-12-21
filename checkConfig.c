6e19c370e55fe16e240468df203772065e024d63
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-21 08:07:57 +0530

    Update checkConfig.c

--- checkConfig.c
+++ checkConfig.c
@@ -5,15 +5,15 @@ char* warningMessage[MAX_WARNING] = {"ReachingMinThreshold","ReachingMaxThreshol
 char* breachMessageLang[MAX_LANG][MAX_MESSAGE] = 
 {
      {
-	     "LOW_SOC_WARNING",
-	     "HIGH_SOC_WARNING",
-	     "LOW_SOC_BREACH",
-	     "HIGH_SOC_BREACH"
+	     "LOW_WARNING",
+	     "HIGH_WARNING",
+	     "LOW_BREACH",
+	     "HIGH_BREACH"
      },
      {
-	     "Niedrige SOC-Warnung",
-	     "Hohe SOC-Warnung",
-	     "Niedrige SOC-Verletzung",
-	     "Hohe SOC-Verletzung"
+	     "Niedrige -Warnung",
+	     "Hohe -Warnung",
+	     "Niedrige -Verletzung",
+	     "Hohe -Verletzung"
      },
 };
